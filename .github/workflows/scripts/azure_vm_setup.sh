3c55e7523c92057249241ec844ad98c98fa1a716
cloudsonika <113979766+cloudsonika@users.noreply.github.com>
2023-11-03 16:04:50 +0530

    Create azure_vm_setup.sh

---
+++ .github/workflows/scripts/azure_vm_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Custom Azure VM setup commands
+
+# Example 1: Update the package list and upgrade installed packages
+sudo apt update
+sudo apt upgrade -y
+
+# Example 2: Install additional software packages
+# sudo apt install -y <package_name>
+
+# Example 3: Configure your application or environment
+# Some setup or configuration specific to your project
